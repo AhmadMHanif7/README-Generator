5a27bda443929116e907f7955ca931c632d984e5
Ahmad Hanif <ahmadmhanif@Ahmads-MBP.lan>
2022-08-09 16:31:28 -0500

    Collect all data

---
+++ Develop/README.md
@@ -0,0 +1,8 @@
+{
+	"title": "Ahmad",
+	"Project Description": "Ahmad",
+	"Installation Instructions": "Ahmad",
+	"Usage Information": "Ahmad",
+	"Contribution Guidelines": "Ahmad",
+	"Test Instructions": "Ahmad"
+}
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,99 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer'); //Imports the inquirer
+const fs = require('fs'); //Imports Fs
+const { isBuffer } = require('util');
+const generateMarkdown  = require('./utils/generateMarkdown') //Imports the generate markdown file
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type:'input',
+    name: 'title',
+    message: 'Enter Project Name:',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a valid name for your project');
+            return false;
+        }
+    }
+    },
+    {
+    type:'input',
+    name: 'Project Description',
+    message: 'Enter Project Description:',
+    validate: nameInput => {
+    if (nameInput) {
+        return true;
+    } else {
+        console.log('Please enter a valid description for your project');
+        return false;
+    }
+    }
+    },
+    {
+    type:'input',
+    name: 'Installation Instructions',
+    message: 'Enter Installation Instructions for the Project:',
+    validate: nameInput => {
+    if (nameInput) {
+        return true;
+    } else {
+        console.log('Please enter a valid installation instructions for your project');
+        return false;
+    }
+}
+    },
+    {
+    type:'input',
+    name: 'Usage Information',
+    message: 'How is this Project used?',
+    validate: nameInput => {
+    if (nameInput) {
+        return true;
+    } else {
+        console.log('Please enter a valid use for your project');
+        return false;
+    }
+}
+    },
+    {
+    type:'input',
+    name: 'Contribution Guidelines',
+    message: 'How can further contribution be made to this applications?',
+    validate: nameInput => {
+    if (nameInput) {
+        return true;
+    } else {
+        console.log('Please enter a valid contribution guidelines for your project');
+        return false;
+    }
+}
+    },
+    {
+    type:'input',
+    name: 'Test Instructions',
+    message: 'Please insert the test instructions for this project?',
+    validate: nameInput => {
+    if (nameInput) {
+        return true;
+    } else {
+        console.log('Please enter a valid test instructions for your project');
+        return false;
+    }
+}  
+    }
+];
+
+
+
+inquirer.prompt(questions).then(data => writeToFile('README.md',data));
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t') , err =>
+        err ? console.log(err) : console.log(data))
+    };
 
 // TODO: Create a function to initialize app
 function init() {}
